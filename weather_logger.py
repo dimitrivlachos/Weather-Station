755711b06bb41da6f2d3b0cfcc73894816e26098
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-02-24 20:11:08 +0000

    Created simple weather logger
    Created a simple python script to poll weather data from the server and log it in a .csv

---
+++ weather_logger.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import csv
+import time
+import os
+
+# Get data from weather station
+def get_json(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return json.loads(r.text)
+    else:
+        print(f'Error: {r.status_code}')
+
+# Save data to csv file with timestamp
+def save_to_csv(data, filename):
+    # check if file exists
+    file_exists = os.path.isfile(filename)
+    with open(filename, 'a') as f:
+        writer = csv.writer(f)
+        # write header row if file is new
+        if not file_exists:
+            writer.writerow(['timestamp', 'temperature', 'humidity', 'pressure'])
+        row = [time.strftime("%Y-%m-%d %H:%M:%S"), data['Readings']['temperature'], data['Readings']['humidity'], data['Readings']['pressure']]
+        writer.writerow(row)
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    url = 'http://192.168.0.190/json'
+    filename = "weather.csv"
+    while True:
+        data = get_json(url)
+        save_to_csv(data, filename)
+        print(f'Data saved to {filename} at {time.strftime("%H:%M:%S")}')
+        time.sleep(1800)  # wait 30 minutes (1800 seconds)
