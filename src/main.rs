5608392c8003ed2b5f39dfe0fe7486402419f102
Dimitri Vlachos <86776716+dimitrivlachos@users.noreply.github.com>
2023-02-28 12:36:29 +0000

    Greatly improved rust script
    Fixed error propagation and weird 'result' usage

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use serde::{Serialize, Deserialize};
+use color_eyre::{self, Result};
 use chrono::{DateTime, Utc};
 use std::fs::OpenOptions;
-use std::io::{Write, Result};
+use std::io::{Write};
 use std::path::Path;
 use reqwest;
 
@@ -14,17 +15,21 @@ struct Readings {
 }
 
 // Makes a request to the ESP8266 and returns the readings
-async fn make_request() -> std::result::Result<Readings, reqwest::Error> {
+async fn make_request() -> Result<Readings> {
     let response = reqwest::Client::new()
     .get("http://192.168.0.190/json")
     .send()
     .await?;
 
     let json_str = response.text().await?;
-    let json: serde_json::Value = serde_json::from_str(&json_str).unwrap();
-    let reading: Readings = serde_json::from_value(json["Readings"].clone()).unwrap();
-
-    Ok(reading)
+    
+    #[derive(Deserialize)]
+    struct ApiResponse {
+        #[serde(rename = "Readings")]
+        readings: Readings
+    }
+    let json: ApiResponse = serde_json::from_str(&json_str)?;
+    Ok(json.readings)
 }
 
 // Writes the data obtained from the make_request() function to a CSV file
@@ -52,8 +57,11 @@ fn write_to_csv(data: &Readings) -> Result<()> {
 }
 
 #[tokio::main]
-async fn main(){    
+async fn main() -> Result<()> {    
+    color_eyre::install()?;
     // Make the request and write the data to the CSV file
-    let reading = make_request().await.unwrap();
-    _ = write_to_csv(&reading);
+    let reading = make_request().await?;
+    write_to_csv(&reading)?;
+
+    Ok(())
 }
